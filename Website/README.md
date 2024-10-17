28aa88c9fb861e94ab0d59edee46ff0f7fb1b5bb
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-10-18 01:10:24 +0530

    Added all necessary file structure

--- Website/README.md
+++
@@ -1,1 +0,0 @@
-## This is Web App for ParKing
---
+++ Website/client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Header from './components/Header';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
