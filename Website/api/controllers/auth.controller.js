91e043ade0c881564cc8b364ccf9a085ea2770d0
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-10-18 14:00:53 +0530

    singup api route created

--- Website/api/controllers/auth.controller.js
+++ Website/api/controllers/auth.controller.js
@@ -1,3 +1,15 @@
-export const test = (req, res) => {
-    res.send("hello world api");
+import bcryptjs from "bcryptjs";
+import userModel from "../models/user.model.js";
+
+export const signUp = async (req, res, next) => {
+    const {username, email, password, contact} = req.body;
+    console.log(username, email, password, contact)
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new userModel({username, email, password: hashedPassword, contact});
+    try{
+        await newUser.save();
+        res.status(201).json("User created successfully");
+    }catch(error){
+        next(error);
+    }
 }
--- Website/api/routes/auth.route.js
+++ Website/api/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { test } from "../controllers/auth.controller.js";
+import { signUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+router.post("/signup", signUp);
 
 export default router;
