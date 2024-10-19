a3c117c8a245c290a678c0b6264bef4c9929f003
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-10-19 13:10:26 +0530

    created sign in api route

--- Website/api/controllers/auth.controller.js
+++ Website/api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import userModel from "../models/user.model.js";
 import { errorHandeler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
     const {username, email, password, contact} = req.body;
@@ -12,6 +13,24 @@ export const signUp = async (req, res, next) => {
     try{
         await newUser.save();
         res.status(201).json("User created successfully");
+    }catch(error){
+        next(error);
+    }
+};
+
+export const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        return next(errorHandeler(404, "Missing Credentials"));
+    }
+    try{
+        const validUser = await userModel.findOne({email});
+        if(!validUser) return next(errorHandeler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandeler(401, "Wrong Credentials"));
+        const token = jwt.sign({_id: validUser._id, email: validUser.email, username: validUser.username, contact: validUser.contact, avatar: validUser.avatar}, process.env.JWT_SECRECT);
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000}).status(200).json(rest);
     }catch(error){
         next(error);
     }
--- Website/client/src/pages/SignIn.jsx
+++ Website/client/src/pages/SignIn.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Link, useNavigate} from "react-router-dom";
 
 export default function SignIn() {
+
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+  }
+
+  const handleSubmit = async (e) => {
+
+  }
   return (
-    <div>
-      this is signin page
+    <div className='flex justify-center items-center h-screen bg-[#C5FFF8]'>
+      <div className="w-[28rem] p-4 rounded-3xl shadow-2xl bg-white">
+        <h1 className='text-center text-3xl my-7'>Login</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center my-4 p-4 gap-4">
+          <input type="email" placeholder='email' id='email' className='px-4 py-3 rounded-lg  border-2 border-gray-300' autoComplete='off' onChange={handleChange} required />
+          <input type="password" placeholder='password' id='password' className='px-4 py-3 rounded-lg border-2 border-gray-300' autoComplete='off' onChange={handleChange} required />
+          <button className="bg-blue-600 text-xl text-white font-semibold w-full py-2 rounded-lg transition-all duration-300 hover:bg-blue-500 disabled:bg-blue-400">{loading?
+            <div className="w-full h-full flex justify-center items-center">
+              <div className="w-8 h-8 border-4 rounded-full border-t-4 border-t-gray-300 border-white animate-spin"></div> 
+              </div>
+            : 'Login'}</button>
+        </form>
+        <p className='px-6 text-xs font-semibold my-4'>By continuing, you agree to Zeprak's Terms of Service and acknowledge you've read our Privacy Policy.Notice all collection</p>
+        <p className='px-4 text-sm'>Forget Password? <Link to="" className='text-blue-500 font-semibold'>Click Here</Link></p>
+        <div className="flex gap-2 px-4 my-2">
+          <p>Don't have an account?</p>
+          <Link to='/sign-up' className='text-blue-500 font-semibold'>Sign Up</Link>
+        </div>
+      {!error && (
+        <p className="text-red-600 text-sm font-semibold px-4 text-center">{error}</p>
+      )}
+      </div>
     </div>
   )
 }
