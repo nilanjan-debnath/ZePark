4f1f6fe93170f52720bcecb0dd611551e21f0175
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-10-21 11:44:24 +0530

    added basic map structure to the map page

--- Website/client/src/pages/Map.jsx
+++ Website/client/src/pages/Map.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, CircleMarker, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+import 'leaflet/dist/leaflet.css'
 
 export default function Map() {
   return (
-    <div>
-      this is map
-    </div>
+    <MapContainer center={[22.5744, 88.3629]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      <CircleMarker center={[22.5744, 88.3629]} radius={10} color='transparent' fillColor='blue' opacity={5}>
+        <Popup>
+          <h1>Kolkata</h1>
+        </Popup>
+      </CircleMarker>
+
+      <Marker position={[22.5754, 88.4798]}>
+        <Popup>
+          <h1>New Town</h1>
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
